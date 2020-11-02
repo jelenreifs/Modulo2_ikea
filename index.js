f75bbfdc4d2dfc3a0361ae88c8bd53c86a5d3856
REIFS HERNANDO <ereifs@serikat.es>
2020-11-02 17:12:58 +0100

    Metodos PUT y DELETE

--- index.js
+++ index.js
@@ -1,86 +1,168 @@
-
 const express = require("express");
-let almacen = require("./almacen.js")
 const app = express();
+
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+let almacen = require("./almacen")
 
-app.get("/almacen", function (req, res) {
+app.get("/almacen", function(req, res) {
     res.send(almacen)
 })
 
-
-app.get("/almacen/:seccion", function (req, res) {
-  let tipo = req.params.seccion;
-
-  if (almacen[tipo] !== undefined) {
-    res.send(almacen[tipo]);
-  } else {
-    let respuesta = {
-      error: true,
-      mensaje: "Esa sección no existe",
-    };
-    res.send(respuesta);
-  }
-});
-
-/* app.get("/almacen/:seccion", function (req, res) {
+app.get("/almacen/:seccion", function(req, res) {
     let seccion = req.params.seccion;
     seccion = seccion.toLowerCase();
     let boolean = false;
+
     if (seccion == "armarios") {
         res.send(almacen.armarios)
         boolean = true;
-    } else if (seccion = "mesas") {
+    } else if (seccion == "mesas") {
         res.send(almacen.mesas)
         boolean = true;
-    } else if (seccion = "sillas") {
+    } else if (seccion == "sillas") {
         res.send(almacen.sillas)
         boolean = true;
     }
+
     if (boolean == false) {
         res.send({ error: true, mensaje: "No disponemos de esa seccion" })
     }
-}) */
+
+})
 
 app.post("/add", function(req, res) {
     let seccion = req.body.seccion
-   // seccion = seccion.toLowerCase()
+    seccion = seccion.toLowerCase()
     let add = {
-        nombre : req.body.nombre,
-        descripcion : req.body.descripccion,
-        img : req.body.img,
-        precio : req.body.precio,
-  }
-  
+        nombre: nombre = req.body.nombre,
+        descripccion: descripccion = req.body.descripccion,
+        img: img = req.body.img,
+        precio: precio = req.body.precio,
+    }
 
     if (seccion == "armarios") {
         almacen.armarios.push(add)
     } else if (seccion == "mesas") {
-        añmacen.mesas.push(add)
+        almacen.mesas.push(add)
     } else if (seccion == "sillas") {
         almacen.sillas.push(add)
 
   }
+  res.send(almacen)
 
-  res.send(add) 
-  
 })
 
+ app.put("/editar", function(req, res) {
+    let seccion = req.body.seccion
+    let nombre = req.body.nombre
+    let img = req.body.img
+    let descripccion = req.body.descripccion
+    let precio = req.body.precio
 
+    let booleanSeccion = false
+    let booleanProducto = false
+
+    if (seccion == "armarios") {
+        for (let i = 0; i < almacen.armarios.length; i++) {
+            almacen.armarios[i].nombre = nombre
+            almacen.armarios[i].img = img
+            almacen.armarios[i].descripccion = descripccion
+            almacen.armarios[i].precio = precio
+            booleanProducto = true
+        }
+        booleanSeccion = true
+        res.send(almacen)
+    } else if (seccion == "mesas") {
+        for (let i = 0; i < almacen.mesas.length; i++) {
+            almacen.mesas[i].nombre = nombre
+            almacen.mesas[i].img = img
+            almacen.mesas[i].descripccion = descripccion
+            almacen.mesas[i].precio = precio
+            booleanProducto = true
+        }
+        booleanSeccion = true
+        res.send(almacen)
+    } else if (seccion == "sillas") {
+        for (let i = 0; i < almacen.sillas.length; i++) {
+            almacen.sillas[i].nombre = nombre
+            almacen.sillas[i].img = img
+            almacen.sillas[i].descripccion = descripccion
+            almacen.sillas[i].precio = precio
+            booleanProducto = true
+        }
+        booleanSeccion = true
+        res.send(almacen)
+    }
+
+    if (booleanSeccion == false) {
+        res.send({ error: true, mensaje: "No existe esa seccion" })
+    }
+
+    if (booleanProducto == false) {
+        res.send({ error: true, mensaje: "No existe ese producto" })
+    }
+
+})
+
+app.delete("/borrar", function(req, res) {
+    let seccion = req.body.seccion
+    let nombre = req.body.nombre
+
+    let booleanSeccion = false
+    let booleanProducto = false
+
+    if (seccion == "armarios") {
+        for (let i = 0; i < almacen.armarios.length; i++) {
+            if (nombre == almacen.armarios[i].nombre)
+               // almacen.armarios[i].splice(i, 1)
+                 almacen.armarios.splice(i, 1)
+            booleanProducto = true
+        }
+        booleanSeccion = true
+        res.send(almacen)
+ 
+      
+
+
+    } else if (seccion == "mesas") {
+        for (let i = 0; i < almacen.mesas.length; i++) {
+            if (nombre == almacen.mesas[i].nombre)
+                //almacen.mesas[i].splice(i, 1)
+                almacen.mesas.splice(i, 1)
+            booleanProducto = true
+        }
+        booleanSeccion = true
+         res.send(almacen)
+       
+    
+
+
+    } else if (seccion == "sillas") {
+        for (let i = 0; i < almacen.sillas.length; i++) {
+            if (nombre == almacen.sillas[i].nombre)
+                //almacen.sillas[i].splice(i, 1)
+                 almacen.sillas.splice(i, 1)
+            booleanProducto = true
+        }
+        booleanSeccion = true
+           res.send(almacen)
+     
+      
+        
+    }
+
+    if (booleanSeccion == false) {
+        res.send({ error: true, mensaje: "No existe esa seccion" })
+    }
+
+    if (booleanProducto == false) {
+        res.send({ error: true, mensaje: "No existe ese producto" })
+    }
+}) 
 
 app.listen(3000, function() {
   console.log('Escuchando puerto 3000');
 });
-
-
-
-
-
-
-
-
-
-
--- public/index.js
+++ public/index.js
@@ -1,105 +1,177 @@
-let almacenTodos = "";
+let almacenTodo = "";
 
 
-fetch('/almacen')
-    .then(response => response.json())
-    .then(data => {
+function mostrarProductos(data, tipo) {
 
-        // Devuelve un array con los valores correspondientes al objeto
-        let almacenArray = Object.values(data);
+    for (let i = 0; i < data[tipo].length; i++) {
+        almacenTodo += `
+        <div class="producto">
+        <img src="${data[tipo][i].img}" alt="${data[tipo][i].nombre}">
+        <div class="info-producto">
+        <h4>${data[tipo][i].nombre}</h4>
+          <p>${data[tipo][i].descripccion}</p>
+            <p>Precio: ${data[tipo][i].precio}</p>
+            </div>
+            </div>
+        `
+    }
+}
 
-        for (let i = 0; i < almacenArray.length; i++) {
-            for (let j = 0; j < almacenArray.length; j++) {
-                almacenTodos +=
-                    `<div class="producto">
-                        <img src="${almacenArray[i][j].img}" alt="${almacenArray[i][j].nombre}">
-                            <div class="info-producto">
-                                <h4>${almacenArray[i][j].nombre}</h4>
-                                <p>${almacenArray[i][j].descripccion}</p>
-                                <p>Precio: ${almacenArray[i][j].precio}</p>
-                            </div>
-                     </div>`
-            }
-        }
-         document.getElementById('resultado').innerHTML = almacenTodos;
-    })
-        
+function mostrarProducto(data, tipo) {
+
+    for (let i = 0; i < data[tipo].length; i++) {
+        almacenTodo = `
+        <div class="producto">
+        <img src="${data[tipo][i].img}" alt="${data[tipo][i].nombre}">
+        <div class="info-producto">
+        <h4>${data[tipo][i].nombre}</h4>
+          <p>${data[tipo][i].descripccion}</p>
+            <p>Precio: ${data[tipo][i].precio}</p>
+            </div>
+            </div>
+        `
+    }
+}
+
+
+fetch('/almacen').then(function(res) {
+    return res.json();
+}).then(function(data) {
+
+    mostrarProductos(data, "armarios");
+    mostrarProductos(data, "mesas");
+    mostrarProductos(data, "sillas");
+
+    document.getElementById('resultado').innerHTML = almacenTodo;
+})
+
+let seccion
 
 function buscarProducto() {
-    let seccion = document.getElementById("productoBuscar").value;
+    seccion = document.getElementById("productoBuscar").value
     fetch(`/almacen/${seccion}`)
         .then(response => response.json())
         .then(data => {
-            if (data.error) {
-                window.alert(data.mensaje);
-            } else {
-                let productos = "";
-                for (let i = 0; i < data.length; i++) {
-                    productos += `
-                <div class="producto" >
-                    <img src="${data[i].img}" alt="${data[i].nombre}">
-                        <div class="info-producto">
-                            <h4>${data[i].nombre}</h4>
-                            <p>${data[i].descripccion}</p>
-                            <p>Precio: ${data[i].precio}</p>
-                        </div>
-                </div>` ;
-                }
-                document.getElementById("resultado").innerHTML = productos;
-            }
-        })
-}
 
+            let almacenSeccion = "";
+
+            if (data.error == true) {
+                document.getElementById("resultado").innerHTML = `
+                <h2>${data.mensaje}</h2>
+                `
+            } else {
+
+                for (let i = 0; i < data.length; i++) {
+
+                    almacenSeccion += `
+                        <div class="producto">
+                            <img src="${data[i].img}" alt="${data[i].nombre}">
+                            <div class="info-producto">
+                                <h4>${data[i].nombre}</h4>
+                                <p>${data[i].descripccion}</p>
+                                <p>Precio: ${data[i].precio}</p>
+                            </div>
+                        </div>
+                        `
+                }
+                document.getElementById('resultado').innerHTML = almacenSeccion;
+            }
+        });
+}
 
 function addProducto() {
-    let imagen = document.getElementById('fotoProducto').value;
-    let nombre = document.getElementById('nombreProducto').value;
-    let descripcion = document.getElementById('descripcionProducto').value;
-    let precio = parseInt(document.getElementById('precioProducto').value);
-
-    let select = document.getElementById('categorias');
-    let seccion = select.options[select.selectedIndex].value;
-    console.log(seccion);
-
+    let seccion = document.getElementById("categorias").value
+    let nombre = document.getElementById("nombreProducto").value
+    let descripccion = document.getElementById("descripcionProducto").value
+    let img = document.getElementById("fotoProducto").value
+    let precio = parseInt(document.getElementById("precioProducto").value)
+    console.log(seccion)
 
     let add = {
-        imagen,
-        nombre,
-        descripcion,
-        precio
-    };
+        seccion: seccion,
+        nombre: nombre,
+        descripccion: descripccion,
+        img: img,
+        precio: precio
+    }
 
-    fetch('/add', {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(add)
-    })
-        .then(response => response.json())
+    fetch("/add", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(add),
+        })
+        .then(function(res) {
+            return res.json();
+        })
         .then(data => {
-            let almacenArray = Object.values(data);
-
-            let producto = "";
-            
-             for (let i = 0; i < almacenArray.length; i++) {
-            for (let j = 0; j < almacenArray.length; j++) {
-                producto +=
-                    `<div class="producto">
-                        <img src="${almacenArray[i][j].img}" alt="${almacenArray[i][j].nombre}">
-                            <div class="info-producto">
-                                <h4>${almacenArray[i][j].nombre}</h4>
-                                <p>${almacenArray[i][j].descripccion}</p>
-                                <p>Precio: ${almacenArray[i][j].precio}</p>
-                            </div>
-                     </div>`
-            }
-        }
-                document.getElementById('resultado').innerHTML = producto;
-            });
-        
-    
+            mostrarProducto(data, "armarios");
+            mostrarProducto(data, "mesas");
+            mostrarProducto(data, "sillas");
+                document.getElementById('resultado').innerHTML = "";
+                document.getElementById('resultado').innerHTML = almacenTodo;
+        });
 }
 
 
-       
+function editProducto() {
+    let seccion = document.getElementById("categoriasEdit").value
+    let nombre = document.getElementById("nombreEdit").value
+    let descripccion = document.getElementById("descripcionEdit").value
+    let img = document.getElementById("fotoEdit").value
+    let precio = parseInt(document.getElementById("precioEdit").value)
+    console.log(seccion)
+
+
+    let producto = {
+        seccion: seccion,
+        nombre: nombre,
+        descripccion: descripccion,
+        img: img,
+        precio: precio
+    }
+
+
+    fetch('/editar', {
+        method: "PUT",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(producto)
+        })
+        .then(response => response.json())
+        .then(function (data) {
+    
+           mostrarProducto(data, "armarios");
+            mostrarProducto(data, "mesas");
+            mostrarProducto(data, "sillas");
+                document.getElementById('resultado').innerHTML = "";
+            document.getElementById('resultado').innerHTML = almacenTodo;
+             
+        });
+} 
+    
+
+function deleteProducto() {
+    let nombre = document.getElementById("nombreProducto").value
+   
+    let producto = {
+            nombre : nombre,       
+    }
+
+    fetch('/borrar', {
+        method: "DELETE",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(producto)
+        })
+        .then(response => response.json())
+        .then(data => {
+            mostrarProductos(data, "armarios");
+            mostrarProductos(data, "mesas");
+            mostrarProductos(data, "sillas");
+                document.getElementById('resultado').innerHTML = "";
+                document.getElementById('resultado').innerHTML = almacenTodo;
+        });
+}
+
+
+
